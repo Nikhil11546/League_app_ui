7f71ef2fc4f8802900665d73bd4720330dba2d41
Nikhil Talluri <Nikhil.Talluri@JBHunt.com>
2022-09-22 09:58:55 -0500

    basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrgLandingComponent } from './org-landing/org-landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    OrgLandingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/org-landing/org-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-org-landing',
+  templateUrl: './org-landing.component.html',
+  styleUrls: ['./org-landing.component.scss']
+})
+export class OrgLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
